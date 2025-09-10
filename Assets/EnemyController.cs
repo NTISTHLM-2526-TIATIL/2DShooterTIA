d964579ec8ac1182bd191bc3bfb8ab814616a2bd
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2025-09-10 10:37:55 +0200

    Enemy logic fix

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+  [SerializeField]
+  float speed;
+
   void Start()
   {
     Vector2 newPos = new();
     newPos.x = Random.Range(-7f, 7f);
-    newPos.y = Random.Range(-4f, 4f);
+    newPos.y = Camera.main.orthographicSize + 1;
 
     transform.position = newPos;
   }
@@ -14,6 +17,12 @@ public class EnemyController : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
+    Vector2 movement = Vector2.down * speed;
+    transform.Translate(movement * Time.deltaTime);
 
+    if (transform.position.y < -Camera.main.orthographicSize - 1)
+    {
+      Destroy(this.gameObject);
+    }
   }
 }
